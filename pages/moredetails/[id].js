74c6da9aebbbc20d0655d47b0f749b5635be7e83
Ron Levi <ronlevi@outlook.de>
2023-08-02 12:26:05 +0200

    updated moredetailspage without styling

--- pages/moredetails/[id].js
+++ pages/moredetails/[id].js
@@ -1,56 +1,3 @@
-// import {
-//   MoreDetailsLayout,
-//   Header,
-//   Ingredients,
-//   GoBackButton,
-// } from "../../components/moredetails_styled/styled_details";
-// import { useRouter } from "next/router";
-// import useSWR from "swr";
-// import Link from "next/link";
-
-// export default function MoreDetails() {
-//   const router = useRouter();
-//   const { id } = router.query;
-
-//   const { data: recipe, error } = useSWR(`/api/recipes/${id}`, {
-//     revalidateOnMount: true,
-//   });
-
-//   if (error) {
-//     return <h1>Something went wrong!</h1>;
-//   }
-
-//   if (!recipe) {
-//     return <h1>Loading...</h1>;
-//   }
-
-//   return (
-//     <>
-//       <Header>
-//         <Link href="/">
-//           <GoBackButton>back to main</GoBackButton>
-//         </Link>
-//       </Header>
-//       <MoreDetailsLayout>
-//         <Ingredients>
-//           <ul>
-//             <h3>Ingredients</h3>
-//             {recipe.ingredients.map((ingredient, index) => (
-//               <li key={index}>
-//                 {ingredient.quantity} {ingredient.name} - {ingredient.type}
-//               </li>
-//             ))}
-//             <article>
-//               <h3>Instructions</h3>
-//               <p>{recipe.instructions}</p>
-//             </article>
-//           </ul>
-//         </Ingredients>
-//       </MoreDetailsLayout>
-//     </>
-//   );
-// }
-
 import {
   MoreDetailsLayout,
   Header,
@@ -60,8 +7,6 @@ import {
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Link from "next/link";
-
-// ... (gleicher Code wie zuvor)
 
 export default function MoreDetails() {
   const router = useRouter();
