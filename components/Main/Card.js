8d4025776aa7be15b27d226dc220b2a91aebf822
Ron Levi <ronlevi@outlook.de>
2023-08-14 09:24:31 +0200

    finish

--- components/Main/Card.js
+++ components/Main/Card.js
@@ -164,7 +164,7 @@ export default function MainPage() {
 
               <p>{recipe.name}</p>
               <MoreDetailsLink href={`/moredetails/${recipe._id}`}>
-                More details
+                Show Recipe
               </MoreDetailsLink>
 
               <ButtonContainer>
